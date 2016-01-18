ee8282cc1c7c46f115070413ff069c118526128b
supermarkioner <supermarkioner@gmail.com>
2016-01-18 11:52:10 +1100

    update

---
+++ Letter Combinations of a Phone Number.java
@@ -0,0 +1,52 @@
+/*
+   Given a digit string, return all possible letter combinations that the number could represent.
+
+    Link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+    Example: Input:Digit string "23" Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+    Solution: None
+
+    Source: http://www.jiuzhang.com/solutions/letter-combinations-of-a-phone-number/
+*/
+
+public class Solution {
+    public List<String> letterCombinations(String digits) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        if (digits == null || digits.equals("")) {
+            return result;
+        }
+
+        Map<Character, char[]> map = new HashMap<Character, char[]>();
+        map.put('0', new char[] {});
+        map.put('1', new char[] {});
+        map.put('2', new char[] { 'a', 'b', 'c' });
+        map.put('3', new char[] { 'd', 'e', 'f' });
+        map.put('4', new char[] { 'g', 'h', 'i' });
+        map.put('5', new char[] { 'j', 'k', 'l' });
+        map.put('6', new char[] { 'm', 'n', 'o' });
+        map.put('7', new char[] { 'p', 'q', 'r', 's' });
+        map.put('8', new char[] { 't', 'u', 'v'});
+        map.put('9', new char[] { 'w', 'x', 'y', 'z' });
+
+        StringBuilder sb = new StringBuilder();
+        helper(map, digits, sb, result);
+
+        return result;
+    }
+
+    private void helper(Map<Character, char[]> map, String digits, 
+        StringBuilder sb, ArrayList<String> result) {
+        if (sb.length() == digits.length()) {
+            result.add(sb.toString());
+            return;
+        }
+
+        for (char c : map.get(digits.charAt(sb.length()))) {
+            sb.append(c);
+            helper(map, digits, sb, result);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
