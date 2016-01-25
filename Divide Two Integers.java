9c0765b3d544fadb853a24fc410c002c782ff789
supermarkioner <supermarkioner@gmail.com>
2016-01-25 14:13:11 +1100

    update

---
+++ Divide Two Integers.java
@@ -0,0 +1,39 @@
+/*
+    Divide two integers without using multiplication, division and mod operator. If it is overflow, return MAX_INT.
+
+    Link: https://leetcode.com/problems/divide-two-integers/
+
+    Example: None
+
+    Solution: None
+    
+    Source: None
+*/
+
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+             return dividend >= 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        if (dividend == 0) {
+            return 0;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean isNegative = (dividend < 0 && divisor > 0) || 
+                             (dividend > 0 && divisor < 0);
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        int result = 0;
+        while(a >= b){
+            int shift = 0;
+            while(a >= (b << shift)){
+                shift++;
+            }
+            a -= b << (shift - 1);
+            result += 1 << (shift - 1);
+        }
+        return isNegative ? -result : result;
+    }
+}
