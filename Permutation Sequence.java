0b4274ec3a1d16deb15262f5fe1580064fd8b924
supermarkioner <supermarkioner@gmail.com>
2016-01-27 14:07:35 +1100

    update

---
+++ Permutation Sequence.java
@@ -0,0 +1,17 @@
+/*
+    The set [1,2,3,…,n] contains a total of n! unique permutations. By listing and labeling all of the permutations in order.
+    
+    Link: https://leetcode.com/problems/permutation-sequence/
+
+    Example: We get the following sequence (ie, for n = 3):
+		"123"
+		"132"
+		"213"
+		"231"
+		"312"
+		"321"
+
+    Solution: We should simulation the process on how construct permutation.
+    
+    Source: None
+*/
