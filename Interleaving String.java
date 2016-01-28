2a7354dd5f5dfaa04f695e39d67295210484e974
supermarkioner <supermarkioner@gmail.com>
2016-01-28 19:44:15 +1100

    update

---
+++ Interleaving String.java
@@ -0,0 +1,48 @@
+/*
+    Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+    Link: https://leetcode.com/problems/interleaving-string/
+
+    Example: For example,
+		Given:
+		s1 = "aabcc",
+		s2 = "dbbca",
+
+		When s3 = "aadbbcbcac", return true.
+		When s3 = "aadbbbaccc", return false.
+
+    Solution: None
+
+    Source: None
+*/
+
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int len1 = (s1 == null) ? 0 : s1.length();
+        int len2 = (s2 == null) ? 0 : s2.length();
+        int len3 = (s3 == null) ? 0 : s3.length();
+
+        if (len3 != len1 + len2) return false;
+
+        boolean [][] f = new boolean[1 + len1][1 + len2];
+        f[0][0] = true;
+        // s1[i1 - 1] == s3[i1 + i2 - 1] && f[i1 - 1][i2]
+        for (int i = 1; i <= len1; i++) {
+            f[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1) && f[i - 1][0];
+        }
+        // s2[i2 - 1] == s3[i1 + i2 - 1] && f[i1][i2 - 1]
+        for (int i = 1; i <= len2; i++) {
+            f[0][i] = s2.charAt(i - 1) == s3.charAt(i - 1) && f[0][i - 1];
+        }
+        // i1 >= 1, i2 >= 1
+        for (int i1 = 1; i1 <= len1; i1++) {
+            for (int i2 = 1; i2 <= len2; i2++) {
+                boolean case1 = s1.charAt(i1 - 1) == s3.charAt(i1 + i2 - 1) && f[i1 - 1][i2];
+                boolean case2 = s2.charAt(i2 - 1) == s3.charAt(i1 + i2 - 1) && f[i1][i2 - 1];
+                f[i1][i2] = case1 || case2;
+            }
+        }
+
+        return f[len1][len2];
+    }
+}
