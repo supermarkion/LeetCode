618fa13faf1562ab8e2f7e9029f3845fb3a4400c
supermarkioner <supermarkioner@gmail.com>
2016-01-28 19:51:44 +1100

    update

---
+++ Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,83 @@
+/*
+    Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then 
+    right to left for the next level and alternate between).
+
+    Link: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+    Example: For example:
+		Given binary tree {3,9,20,#,#,15,7},
+		    3
+		   / \
+		  9  20
+		    /  \
+		   15   7
+		return its zigzag level order traversal as:
+		[
+		  [3],
+		  [20,9],
+		  [15,7]
+		]
+
+    Solution: None
+
+    Source: None
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> currLevel = new Stack<TreeNode>();
+        Stack<TreeNode> nextLevel = new Stack<TreeNode>();
+        Stack<TreeNode> tmp;
+        
+        currLevel.push(root);
+        boolean normalOrder = true;
+
+        while (!currLevel.isEmpty()) {
+            List<Integer> currLevelResult = new ArrayList<Integer>();
+
+            while (!currLevel.isEmpty()) {
+                TreeNode node = currLevel.pop();
+                currLevelResult.add(node.val);
+
+                if (normalOrder) {
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                } else {
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                }
+            }
+
+            result.add(currLevelResult);
+            tmp = currLevel;
+            currLevel = nextLevel;
+            nextLevel = tmp;
+            normalOrder = !normalOrder;
+        }
+
+        return result;
+    }
+}
