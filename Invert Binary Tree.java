29994f1e8b764997555bb36611af59c44b49b190
supermarkioner <supermarkioner@gmail.com>
2016-01-30 10:49:20 +1100

    update

---
+++ Invert Binary Tree.java
@@ -0,0 +1,23 @@
+/*
+    Invert a binary tree.
+	     4
+	   /   \
+	  2     7
+	 / \   / \
+	1   3 6   9
+	to
+	     4
+	   /   \
+	  7     2
+	 / \   / \
+	9   6 3   1
+
+    
+    Link: https://leetcode.com/problems/invert-binary-tree/
+
+    Example: None
+
+    Solution: None
+    
+    Source: None
+*/
