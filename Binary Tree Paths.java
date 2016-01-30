ab5655c9fcd857d5364a3e86d2b0a193a489ba78
supermarkioner <supermarkioner@gmail.com>
2016-01-31 10:20:11 +1100

    update

---
+++ Binary Tree Paths.java
@@ -0,0 +1,58 @@
+/*
+    Given a binary tree, return all root-to-leaf paths.
+    
+    Link: https://leetcode.com/problems/binary-tree-paths/
+
+    Example: For example, given the following binary tree:
+		   1
+		 /   \
+		2     3
+		 \
+		  5
+		All root-to-leaf paths are:
+
+		["1->2->5", "1->3"]
+
+    Solution: None
+    
+    Source: None
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<String>();
+        if (root == null) {
+            return result;
+        }
+        helper(root, String.valueOf(root.val), result);
+        return result;
+    }
+    
+    private void helper(TreeNode root, String path, List<String> result) {
+        if (root == null) {
+            return;
+        }
+        
+        if (root.left == null && root.right == null) {
+            result.add(path);
+            return;
+        }
+        
+        if (root.left != null) {
+            helper(root.left, path + "->" + String.valueOf(root.left.val), result);
+        }
+        
+        if (root.right != null) {
+            helper(root.right, path + "->" + String.valueOf(root.right.val), result);
+        }
+    }
+}
