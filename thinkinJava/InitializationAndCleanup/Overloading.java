0fd320723d2fad51055c36ecdfedfcfca76f369f
supermarkioner <supermarkioner@gmail.com>
2016-02-04 13:49:24 +1100

    update

---
+++ thinkinJava/InitializationAndCleanup/Overloading.java
@@ -0,0 +1,60 @@
+package InitializationAndCleanup;
+
+//: initialization/Overloading.java
+// Demonstration of both constructor
+// and ordinary method overloading.
+
+/* the same word expresses a number of different meanings—it’s overloaded.
+This is useful, especially when it comes to trivial differences.
+Here’s an example that shows both overloaded constructors and overloaded methods
+*/
+
+class Tree {
+    int height;
+    Tree() {
+        System.out.println("Planting a seedling");
+        height = 0;
+    }
+    Tree(int initialHeight) {
+        height = initialHeight;
+        System.out.println("Creating new Tree that is " + height + " feet tall");
+    }
+    void info() {
+        System.out.println("Tree is " + height + " feet tall");
+    }
+    void info(String s) {
+        System.out.println(s + ": Tree is " + height + " feet tall");
+    }
+}
+
+/**
+ * Created by saima on 4/02/16.
+ */
+public class Overloading {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Tree t = new Tree(i);
+            t.info();
+            t.info("overloaded method");
+        }
+        // Overloaded constructor:
+        new Tree();
+    }
+} /* Output:
+Creating new Tree that is 0 feet tall
+Tree is 0 feet tall
+overloaded method: Tree is 0 feet tall
+Creating new Tree that is 1 feet tall
+Tree is 1 feet tall
+overloaded method: Tree is 1 feet tall
+Creating new Tree that is 2 feet tall
+Tree is 2 feet tall
+overloaded method: Tree is 2 feet tall
+Creating new Tree that is 3 feet tall
+Tree is 3 feet tall
+overloaded method: Tree is 3 feet tall
+Creating new Tree that is 4 feet tall
+Tree is 4 feet tall
+overloaded method: Tree is 4 feet tall
+Planting a seedling
+*///:~
