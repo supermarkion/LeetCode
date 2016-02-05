9ac5ba694b057f7b434ebe898a0c7a7da477b95d
supermarkioner <supermarkioner@gmail.com>
2016-02-05 15:17:27 +1100

    update

--- thinkinJava/AccessControl/InterfaceDemo.java
+++ thinkinJava/AccessControl/InterfaceDemo.java
@@ -7,20 +7,21 @@ package AccessControl;
 interface Flyanimal{
     void fly();
 }
-class   Insect {
-    int  legnum=6;
+class Insect {
+    int legnum = 6;
 }
-class  Bird {
-    int  legnum=2;
+class Bird {
+    int  legnum = 2;
     void egg(){};
 }
-class Ant extends Insect implements  Flyanimal {
+
+class Ant extends Insect implements Flyanimal {
     public void fly() {
-        System.out.println("Ant can  fly");
+        System.out.println("Ant can fly");
     }
 }
 
-class Pigeon extends Bird implements  Flyanimal {
+class Pigeon extends Bird implements Flyanimal {
     public void fly() {
         System.out.println("pigeon  can fly");
     }
@@ -30,11 +31,11 @@ class Pigeon extends Bird implements  Flyanimal {
 }
 public class InterfaceDemo{
     public static void main(String args[]){
-            Ant a=new Ant();
-            a.fly();
-            System.out.println("Ant's legs are"+ a.legnum);
-            Pigeon p= new Pigeon();
-            p.fly();
-            p.egg();
-        }
+        Ant a = new Ant();
+        a.fly();
+        System.out.println("Ant's legs are" + a.legnum);
+        Pigeon p = new Pigeon();
+        p.fly();
+        p.egg();
+    }
 }
