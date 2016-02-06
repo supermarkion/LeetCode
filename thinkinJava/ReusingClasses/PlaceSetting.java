710a524dbeb37c6974d8d4b72bf44c51997f9922
supermarkioner <supermarkioner@gmail.com>
2016-02-06 12:34:18 +1100

    update

---
+++ thinkinJava/ReusingClasses/PlaceSetting.java
@@ -0,0 +1,92 @@
+package ReusingClasses;
+
+/*
+It is very common to use composition and inheritance together. The following example shows
+the creation of a more complex class, using both inheritance and composition, along with the
+necessary constructor initialization
+
+*/
+
+//: reusing/PlaceSetting.java
+// Combining composition & inheritance.
+
+class Plate {
+    Plate(int i) {
+        System.out.println("Plate constructor");
+    }
+}
+
+class DinnerPlate extends Plate {
+    DinnerPlate(int i) {
+        super(i);
+        System.out.println("DinnerPlate constructor");
+    }
+}
+
+class Utensil {
+    Utensil(int i) {
+        System.out.println("Utensil constructor");
+    }
+}
+
+class Spoon extends Utensil {
+    Spoon(int i) {
+        super(i);
+        System.out.println("Spoon constructor");
+    }
+}
+
+class Fork extends Utensil {
+    Fork(int i) {
+        super(i);
+        System.out.println("Fork constructor");
+    }
+}
+
+class Knife extends Utensil {
+    Knife(int i) {
+        super(i);
+        System.out.println("Knife constructor");
+    }
+}
+
+// A cultural way of doing something
+class Custom {
+    Custom(int i) {
+        System.out.println("Custom constructor");
+    }
+}
+
+/**
+ * Created by saima on 6/02/16.
+ */
+public class PlaceSetting extends Custom{
+    private Spoon sp;
+    private Fork frk;
+    private Knife kn;
+    private DinnerPlate pl;
+
+    public PlaceSetting(int i) {
+        super(i + 1);
+        sp = new Spoon(i + 2);
+        frk = new Fork(i + 3);
+        kn = new Knife(i + 4);
+        pl = new DinnerPlate(i + 5);
+        System.out.println("PlaceSetting constructor");
+    }
+
+    public static void main(String[] args) {
+        PlaceSetting x = new PlaceSetting(9);
+    }
+}  /* Output:
+Custom constructor
+Utensil constructor
+Spoon constructor
+Utensil constructor
+Fork constructor
+Utensil constructor
+Knife constructor
+Plate constructor
+DinnerPlate constructor
+PlaceSetting constructor
+*///:~
