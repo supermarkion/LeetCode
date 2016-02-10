440926416fc876a7a61b581a43e76134b364989a
supermarkioner <supermarkioner@gmail.com>
2016-02-10 12:56:56 +1100

    update

--- thinkinJava/innerClasses/Sequence.java
+++ thinkinJava/innerClasses/Sequence.java
@@ -15,24 +15,36 @@ interface Selector {
 public class Sequence {
     private Object[] items;
     private int next = 0;
-    public Sequence(int size) { items = new Object[size]; }
+    public Sequence(int size) {
+        items = new Object[size];
+    }
     public void add(Object x) {
-        if(next < items.length)
+        if (next < items.length) {
             items[next++] = x;
+        }
     }
     private class SequenceSelector implements Selector {
         private int i = 0;
-        public boolean end() { return i == items.length; }
-        public Object current() { return items[i]; }
-        public void next() { if(i < items.length) i++; }
+        public boolean end() {
+            return i == items.length;
+        }
+        public Object current() {
+            return items[i];
+        }
+        public void next() {
+            if(i < items.length) {
+                i++;
+            }
+        }
     }
     public Selector selector() {
         return new SequenceSelector();
     }
     public static void main(String[] args) {
         Sequence sequence = new Sequence(10);
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < 10; i++) {
             sequence.add(Integer.toString(i));
+        }
         Selector selector = sequence.selector();
         while(!selector.end()) {
             System.out.print(selector.current() + " ");
