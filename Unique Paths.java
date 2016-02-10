4b41e6588985f0ad52972afbe4df3ea867fa38ae
supermarkioner <supermarkioner@gmail.com>
2016-02-10 11:33:35 +1100

    update

---
+++ Unique Paths.java
@@ -0,0 +1,35 @@
+/*
+    A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+	The robot can only move either down or right at any point in time. The robot is trying to reach 
+	the bottom-right corner of the grid (marked 'Finish' in the diagram below). How many possible 
+	unique paths are there?
+
+    Link: https://leetcode.com/problems/unique-paths/
+    
+    Example: None
+
+    Solution: Typical DP problem.
+    
+    Source: None
+*/
+
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m < 1 || n < 1) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
