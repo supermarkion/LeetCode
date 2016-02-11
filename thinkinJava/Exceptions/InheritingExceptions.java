b8c3732cde359644a2cb21a8960b7c3882b80757
supermarkioner <supermarkioner@gmail.com>
2016-02-11 18:48:54 +1100

    update

---
+++ thinkinJava/Exceptions/InheritingExceptions.java
@@ -0,0 +1,27 @@
+package Exceptions;
+
+//: exceptions/InheritingExceptions.java
+// Creating your own exceptions.
+
+class SimpleException extends Exception {}
+
+/**
+ * Created by saima on 11/02/16.
+ */
+public class InheritingExceptions {
+    public void f() throws SimpleException {
+        System.out.println("Throw SimpleException from f()");
+        throw new SimpleException();
+    }
+    public static void main(String[] args) {
+        InheritingExceptions sed = new InheritingExceptions();
+        try {
+            sed.f();
+        } catch (SimpleException e) {
+            System.out.println("Caught it!");
+        }
+    }
+} /* Output:
+Throw SimpleException from f()
+Caught it!
+*///:~
