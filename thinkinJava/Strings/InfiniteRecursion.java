e2d9f33e9f9e452793469bfbc3014d94134db55e
supermarkioner <supermarkioner@gmail.com>
2016-02-12 12:53:12 +1100

    update

---
+++ thinkinJava/Strings/InfiniteRecursion.java
@@ -0,0 +1,22 @@
+package Strings;
+
+//: strings/InfiniteRecursion.java
+// Accidental recursion.
+// {RunByHand}
+import java.util.*;
+
+/**
+ * Created by saima on 12/02/16.
+ */
+public class InfiniteRecursion {
+    public String toString() {
+        return " InfiniteRecursion address: " + this + "\n";
+    }
+    public static void main(String[] args) {
+        List<InfiniteRecursion> v =
+                new ArrayList<InfiniteRecursion>();
+        for(int i = 0; i < 10; i++)
+            v.add(new InfiniteRecursion());
+        System.out.println(v);
+    }
+} ///:~
