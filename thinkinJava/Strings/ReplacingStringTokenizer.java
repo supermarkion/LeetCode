40a28deb4724b7bc8726215a9f6986ad0177ea38
supermarkioner <supermarkioner@gmail.com>
2016-02-12 13:05:17 +1100

    update

---
+++ thinkinJava/Strings/ReplacingStringTokenizer.java
@@ -0,0 +1,24 @@
+package Strings;
+
+import java.util.*;
+
+/**
+ * Created by saima on 12/02/16.
+ */
+public class ReplacingStringTokenizer {
+    public static void main(String[] args) {
+        String input = "But I’m not dead yet! I feel happy!";
+        StringTokenizer stoke = new StringTokenizer(input);
+        while(stoke.hasMoreElements())
+            System.out.print(stoke.nextToken() + " ");
+        System.out.println();
+        System.out.println(Arrays.toString(input.split(" ")));
+        Scanner scanner = new Scanner(input);
+        while(scanner.hasNext())
+            System.out.print(scanner.next() + " ");
+    }
+} /* Output:
+But I’m not dead yet! I feel happy!
+[But, I’m, not, dead, yet!, I, feel, happy!]
+But I’m not dead yet! I feel happy!
+*///:~
