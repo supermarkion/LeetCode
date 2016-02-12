5f4298c60cc97e3648235e1506574dd81b2889e3
supermarkioner <supermarkioner@gmail.com>
2016-02-12 13:00:33 +1100

    update

---
+++ thinkinJava/Strings/TestRegularExpression.java
@@ -0,0 +1,44 @@
+package Strings;
+
+//: strings/TestRegularExpression.java
+// Allows you to easily try out regular expressions.
+// {Args: abcabcabcdefabc "abc+" "(abc)+" "(abc){2,}" }
+
+import java.util.regex.*;
+
+/**
+ * Created by saima on 12/02/16.
+ */
+public class TestRegularExpression {
+    public static void main(String[] args) {
+        if(args.length < 2) {
+            System.out.println("Usage:\njava TestRegularExpression " +
+                    "characterSequence regularExpression+");
+            System.exit(0);
+        }
+        System.out.println("Input: \"" + args[0] + "\"");
+        for(String arg : args) {
+            System.out.println("Regular expression: \"" + arg + "\"");
+            Pattern p = Pattern.compile(arg);
+            Matcher m = p.matcher(args[0]);
+            while(m.find()) {
+                System.out.println("Match \"" + m.group() + "\" at positions " +
+                        m.start() + "-" + (m.end() - 1));
+            }
+        }
+    }
+} /* Output:
+Input: "abcabcabcdefabc"
+Regular expression: "abcabcabcdefabc"
+Match "abcabcabcdefabc" at positions 0-14
+Regular expression: "abc+"
+Match "abc" at positions 0-2
+Match "abc" at positions 3-5
+Match "abc" at positions 6-8
+Match "abc" at positions 12-14
+Regular expression: "(abc)+"
+Match "abcabcabc" at positions 0-8
+Match "abc" at positions 12-14
+Regular expression: "(abc){2,}"
+Match "abcabcabc" at positions 0-8
+*///:~
