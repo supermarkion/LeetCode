65a1b19d21bf3c38dd30dd2a6cb2503ddd89edbb
supermarkioner <supermarkioner@gmail.com>
2016-02-16 13:50:02 +1100

    update

---
+++ thinkinJava/generics/Holder3.java
@@ -0,0 +1,23 @@
+package generics;
+
+//: generics/Holder3.java
+
+/**
+ * Created by saima on 16/02/16.
+ */
+public class Holder3<T> {
+    private T a;
+    public Holder3(T a) {
+        this.a = a;
+    }
+    public void set(T a) {
+        this.a = a;
+    }
+    public T get() {
+        return a;
+    }
+    public static void main(String[] args) {
+        Holder3<Automobile> h3 = new Holder3<Automobile>(new Automobile());
+        Automobile a = h3.get();        // Not cast needed
+    }
+}
