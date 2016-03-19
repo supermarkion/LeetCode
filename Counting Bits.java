59a46ca0ea78eea6d4585a8287f4faeb80c4f809
supermarkioner <supermarkioner@gmail.com>
2016-03-19 11:58:22 +1100

    update

--- Counting Bits.java
+++ Counting Bits.java
@@ -6,8 +6,32 @@
 
     Example: For num = 5 you should return [0,1,1,2,1,2].
 
-    Solution: None
+    Solution: The main point is that next block is current block + [1 + j for i in current block].
 
-    Source: None
+    Source: https://leetcode.com/discuss/92824/share-my-4ms-ac-java-solution
 */
 
+public class Solution {
+    int[] ans;
+    public int[] countBits(int num) {
+        ans = new int[num + 1];
+        if (num == 0) {
+        	return ans;
+        }
+
+        countBitsHelper(num, 1, 2);
+        return ans;
+    }
+    
+    private void countBitsHelper(int num, int start, int end) {
+        int i;
+        for (i = start; i < end && i <= num; i++) {
+            ans[i] = 1 + ans[i - start];				// the next block is last block plus 1 and repeat
+        }
+        if (end <= num) {
+        	countBitsHelper(num, i, 2 * i);
+        } else {
+        	return;
+        }
+    }
+}
