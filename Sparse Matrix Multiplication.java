ed9195170d4354989daf2a22dd5a402cab0334bf
supermarkioner <supermarkioner@gmail.com>
2016-04-04 08:55:01 +1000

    update

---
+++ Sparse Matrix Multiplication.java
@@ -0,0 +1,27 @@
+/*
+    Given two sparse matrices A and B, return the result of AB. You may assume that A's column number is equal to B's row number.
+    
+    Link: https://leetcode.com/problemset/algorithms/
+
+    Example: 
+	    Example:
+			A = [
+			  [ 1, 0, 0],
+			  [-1, 0, 3]
+			]
+
+			B = [
+			  [ 7, 0, 0 ],
+			  [ 0, 0, 0 ],
+			  [ 0, 0, 1 ]
+			]
+
+
+			     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+			AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+			                  | 0 0 1 |
+	
+    Solution: None
+
+    Source: http://www.cnblogs.com/jcliBlogger/p/5015959.html
+*/
