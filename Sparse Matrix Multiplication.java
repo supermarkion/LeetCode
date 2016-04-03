b63ac277d941fc5c47074a7cd6136a853d36c175
supermarkioner <supermarkioner@gmail.com>
2016-04-04 09:23:47 +1000

    update

--- Sparse Matrix Multiplication.java
+++ Sparse Matrix Multiplication.java
@@ -31,10 +31,10 @@ public int[][] multiply(int[][] A, int[][] B) {
     int[][] result = new int[m][nB];
 
     List[] indexA = new List[m];
-    for(int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {
         List<Integer> numsA = new ArrayList<>();
-        for(int j = 0; j < n; j++) {
-            if(A[i][j] != 0){
+        for (int j = 0; j < n; j++) {
+            if (A[i][j] != 0){
                 numsA.add(j); 
                 numsA.add(A[i][j]);
             }
@@ -42,12 +42,12 @@ public int[][] multiply(int[][] A, int[][] B) {
         indexA[i] = numsA;
     }
 
-    for(int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {
         List<Integer> numsA = indexA[i];
-        for(int p = 0; p < numsA.size() - 1; p += 2) {
+        for (int p = 0; p < numsA.size() - 1; p += 2) {
             int colA = numsA.get(p);
             int valA = numsA.get(p + 1);
-            for(int j = 0; j < nB; j ++) {
+            for (int j = 0; j < nB; j ++) {
                 int valB = B[colA][j];
                 result[i][j] += valA * valB;
             }
