58ce363205fa1d9eb40d4c71b2deb71fa3a0ae29
supermarkioner <supermarkioner@gmail.com>
2016-04-08 13:10:27 +1000

    update

--- Range Sum Query 2D - Mutable.java
+++ Range Sum Query 2D - Mutable.java
@@ -19,5 +19,35 @@
 	
     Solution: None
 
-    Source: http://bookshadow.com/weblog/2015/11/18/leetcode-range-sum-query-mutable/
+    Source: http://buttercola.blogspot.com.au/2015/12/leetcode-range-sum-query-2d-immutable.html
 */
+
+public class NumMatrix {
+    private int[][] matrix;
+    private int[][] sum;
+     
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+         
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+         
+        int m = matrix.length;
+        int n = matrix[0].length;
+         
+        sum = new int[m + 1][n + 1];
+         
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - 
+                            sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+ 
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2 + 1][col2 + 1] - sum[row2 + 1][col1] - 
+               sum[row1][col2 + 1] + sum[row1][col1];
+    }
+}
