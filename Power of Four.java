11be526b7fd1f5366bd5fb4b2443f78c3507ce28
supermarkioner <supermarkioner@gmail.com>
2016-04-21 09:00:22 +1000

    update

---
+++ Power of Four.java
@@ -0,0 +1,12 @@
+/*
+    Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+    Link: https://leetcode.com/problems/power-of-four/
+
+    Example: 
+	    Given num = 16, return true. Given num = 5, return false.
+	
+    Solution: None
+
+    Source: None
+*/
