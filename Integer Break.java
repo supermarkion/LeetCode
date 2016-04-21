7c75ac72dbad578d2ac8c210a84cd670a178695d
supermarkioner <supermarkioner@gmail.com>
2016-04-22 08:52:10 +1000

    update

---
+++ Integer Break.java
@@ -0,0 +1,13 @@
+/*
+    Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. 
+    Return the maximum product you can get. 
+    
+    Link: https://leetcode.com/problems/integer-break/
+
+    Example: 
+	    For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4)
+	
+    Solution: None
+
+    Source: None
+*/
