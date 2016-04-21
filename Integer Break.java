4d8737f474bcb6faf09dee00fe3c96bb801616bc
supermarkioner <supermarkioner@gmail.com>
2016-04-22 09:00:50 +1000

    update

--- Integer Break.java
+++ Integer Break.java
@@ -1,7 +1,7 @@
 /*
     Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. 
     Return the maximum product you can get. 
-    
+
     Link: https://leetcode.com/problems/integer-break/
 
     Example: 
@@ -11,3 +11,20 @@
 
     Source: None
 */
+
+public class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+
+        int mod = n % 3;
+        
+        if (mod == 0) {
+            return (int) Math.pow(3, n / 3);
+        } else if (mod == 1) {
+            return 4 * (int) Math.pow(3, (n - 4) / 3);
+        }
+        return 2 * (int) Math.pow(3, n / 3);
+    }
+}
