ebb984896f734555677422171881e7d2812b685c
supermarkioner <supermarkioner@gmail.com>
2016-05-05 09:09:16 +1000

    update

---
+++ Missing Ranges.java
@@ -0,0 +1,31 @@
+/*
+    Given a sorted integer array where the range of elements are [0, 99] inclusive, return its missing ranges.
+    
+    Link: https://leetcode.com/problemset/algorithms/
+
+    Example: 
+        For example, given [0, 1, 3, 50, 75], return [“2”, “4->49”, “51->74”, “76->99”]
+    
+    Solution: None
+
+    Source: http://www.danielbit.com/blog/puzzle/leetcode/leetcode-missing-ranges
+*/
+
+public class Solution {
+    public List<String> findMissingRanges(int[] vals, int start, int end) {
+        List<String> ranges = new ArrayList<String>();
+        int prev = start - 1;
+        for (int i = 0; i <= vals.length; ++i) {
+            int curr = (i == vals.length) ? end + 1 : vals[i];
+            if ( cur - prev >= 2 ) {
+                ranges.add(getRange(prev + 1, curr - 1));
+            }
+            prev = curr;
+        }
+        return ranges;
+    }
+ 
+    private String getRange(int from, int to) {
+        return (from == to) ? String.valueOf(from) : from + "->" + to;
+    }
+}
