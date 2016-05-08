4ffb93fa5c792cef1d9999a05ee1ff5e97f19c7a
SaiMa <supermarkioner@gmail.com>
2016-05-08 10:28:05 +1000

    update

---
+++ Shortest Word Distance.java
@@ -0,0 +1,15 @@
+/*
+    Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+    Link: https://leetcode.com
+
+    Example: 
+    	Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+		Given word1 = “coding”, word2 = “practice”, return 3.
+		Given word1 = "makes", word2 = "coding", return 1.
+
+    Solution: None
+
+    Source: http://blog.csdn.net/xudli/article/details/48766955
+*/
+
