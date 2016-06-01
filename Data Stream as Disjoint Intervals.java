f8fb8511cbe14c17a5f0072d1998e5a38c99db0a
supermarkioner <supermarkioner@gmail.com>
2016-06-01 12:08:47 +1000

    update

---
+++ Data Stream as Disjoint Intervals.java
@@ -0,0 +1,17 @@
+/*
+    Given a data stream input of non-negative integers a1, a2, ..., an, ..., summarize the numbers seen so far as a list of disjoint intervals.
+
+    Link: https://leetcode.com/problems/data-stream-as-disjoint-intervals/
+
+    Example:  
+        For example, suppose the integers from the data stream are 1, 3, 7, 2, 6, ..., then the summary will be:
+		[1, 1]
+		[1, 1], [3, 3]
+		[1, 1], [3, 3], [7, 7]
+		[1, 3], [7, 7]
+		[1, 3], [6, 7]
+    
+    Solution: None
+
+    Source: None
+*/
