906e873996ebdd95ac9aa49bb8ca3c685aa02f33
supermarkioner <supermarkioner@gmail.com>
2016-06-14 12:23:36 +1000

    update

---
+++ Count Numbers with Unique Digits.java
@@ -0,0 +1,12 @@
+/*
+    Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+
+    Link: https://leetcode.com/problems/count-numbers-with-unique-digits/
+
+    Example: 
+    	Given n = 2, return 91. (The answer should be the total numbers in the range of 0 ≤ x < 100, excluding [11,22,33,44,55,66,77,88,99])
+    
+    Solution: None
+
+    Source: None
+*/
