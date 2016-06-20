b4f65b912db7fc55342b66b69a4712011040de25
supermarkioner <supermarkioner@gmail.com>
2016-06-21 08:55:25 +1000

    update

--- Rearrange String k Distance Apart.java
+++ Rearrange String k Distance Apart.java
@@ -27,5 +27,39 @@
     
     Solution: None
 
-    Source: None
+    Source: https://leetcode.com/discuss/108530/java-15ms-solution-with-two-auxiliary-array-o-n-time
 */
+
+public class Solution {
+    public String rearrangeString(String str, int k) {
+        int length = str.length();
+        int[] count = new int[26];
+        int[] valid = new int[26];
+        for (int i = 0; i < length; i++) {
+            count[str.charAt(i) - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int index = 0;index < length;index++) {
+            int candidatePos = findValidMax(count, valid, index);
+            if (candidatePos == -1) {
+            	return "";	
+            }
+            count[candidatePos]--;
+            valid[candidatePos] = index + k;
+            sb.append((char)('a' + candidatePos));
+        }
+        return sb.toString();
+    }
+
+   private int findValidMax(int[] count, int[] valid, int index){
+       int max = Integer.MIN_VALUE;
+       int candidatePos = -1;
+       for (int i = 0; i < count.length; i++) {
+           if (count[i] > 0 && count[i] > max && index >= valid[i]) {
+               max = count[i];
+               candidatePos = i;
+           }
+       }
+       return candidatePos;
+   }
+}
