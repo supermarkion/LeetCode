183b0ed772d82d13d5c30f50fb1b6f5db62111ed
supermarkioner <supermarkioner@gmail.com>
2016-06-23 09:21:39 +1000

    update

---
+++ Max Sum of Rectangle No Larger Than K.java
@@ -0,0 +1,17 @@
+/*
+    Given a non-empty 2D matrix matrix and an integer k, find the max sum of a rectangle in the matrix such that its sum is no larger than k.
+
+    Link: https://leetcode.com/problems/max-sum-of-sub-matrix-no-larger-than-k/
+
+    Example: 
+		Given matrix = [
+		  [1,  0, 1],
+		  [0, -2, 3]
+		]
+		k = 2
+		The answer is 2. Because the sum of rectangle [[0, 1], [-2, 3]] is 2 and 2 is the max number no larger than k (k = 2).
+    
+    Solution: None
+
+    Source: None
+*/
