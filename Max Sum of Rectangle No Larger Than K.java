29e9db3cbd6145aed564e1b023d2a27f26c08fad
SaiMa <supermarkioner@gmail.com>
2016-06-25 11:37:35 +1000

    update

--- Max Sum of Rectangle No Larger Than K.java
+++ Max Sum of Rectangle No Larger Than K.java
@@ -13,5 +13,40 @@
     
     Solution: None
 
-    Source: None
+    Source: https://leetcode.com/discuss/110363/java-244ms-applying
 */
+
+public class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+    //Assuming that rows is larger than the number of cols
+
+    int row = matrix.length, col = matrix[0].length;
+    int minDf = Integer.MAX_VALUE;
+    for (int left = 0; left < col; left++) {
+        int[] tmp = new int[row];
+        for (int right = left; right < col; right++) {
+            TreeSet<Integer> set = new TreeSet<>();
+            int cursum = 0;
+            for (int i = 0; i < row; i++) {
+                tmp[i] += matrix[i][right];
+                cursum += tmp[i];
+                if (cursum == k) {
+                	return k;
+                }
+                if (cursum < k) {
+                	minDf = Math.min(minDf, k - cursum);
+                } 
+                Integer x = set.ceiling(cursum - k);
+                if (x != null && cursum - x <= k) {
+                	minDf = Math.min(minDf, k - cursum + x);
+                }
+                if (minDf == 0) {
+                	return k;
+                }
+                set.add(cursum);
+            }
+        }
+    }
+    return k - minDf;
+    }
+}
