f02bb6c7451fc5c6c6eee29d6797ce18a7e6f5f5
supermarkioner <supermarkioner@gmail.com>
2016-06-29 08:47:58 +1000

    update

--- Water and Jug Problem.java
+++ Water and Jug Problem.java
@@ -21,5 +21,30 @@
     
     Solution: None
 
-    Source: None
+    Source: https://leetcode.com/discuss/110478/math-solution-java-solution
 */
+
+public class Solution {
+    public boolean canMeasureWater(int x, int y, int z) {
+    //limit brought by the statement that water is finallly in one or both buckets
+	    if (x + y < z) {
+	    	return false;	
+	    }
+	    //case x or y is zero
+	    if (x == z || y == z || x + y == z) {
+	    	return true;	
+	    } 
+
+	    //get GCD, then we can use the property of Bézout's identity
+	    return z % GCD(x, y) == 0;
+    }
+
+    public int GCD(int a, int b){
+        while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
