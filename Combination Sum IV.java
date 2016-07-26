d2fc69abb77576e34cb132c2c92dca86de219a0a
supermarkioner <supermarkioner@gmail.com>
2016-07-27 08:59:41 +1000

    update

---
+++ Combination Sum IV.java
@@ -0,0 +1,26 @@
+/*
+    Given an integer array with all positive numbers and no duplicates, find the number of 
+    possible combinations that add up to a positive integer target.
+    
+    Link: https://leetcode.com/problems/combination-sum-iv/
+
+    Example:
+        nums = [1, 2, 3]
+		target = 4
+
+		The possible combination ways are:
+		(1, 1, 1, 1)
+		(1, 1, 2)
+		(1, 2, 1)
+		(1, 3)
+		(2, 1, 1)
+		(2, 2)
+		(3, 1)
+
+		Note that different sequences are counted as different combinations.
+		Therefore the output is 7.
+
+    Solution: None
+
+    Source: None
+*/
