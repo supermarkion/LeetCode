5c25dd7fb6540786aabf42493e2594c3d15ebd3b
supermarkioner <supermarkioner@gmail.com>
2016-07-28 08:58:59 +1000

    update

--- Combination Sum IV.java
+++ Combination Sum IV.java
@@ -22,5 +22,20 @@
 
     Solution: None
 
-    Source: None
+    Source: https://leetcode.com/problems/combination-sum-iv/
 */
+
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] comb = new int[target + 1];
+        comb[0] = 1;
+        for (int i = 1; i < comb.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    comb[i] += comb[i - nums[j]];
+                }
+            }
+        }
+        return comb[target];
+    }
+}
