49ed4de00bdd1ea7faeea7bc8ec71d2bb5f20582
supermarkioner <supermarkioner@gmail.com>
2016-08-10 09:13:04 +1000

    update

--- Kth Smallest Element in a Sorted Matrix.java
+++ Kth Smallest Element in a Sorted Matrix.java
@@ -16,5 +16,37 @@
     
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/52948/share-my-thoughts-and-clean-java-code/2
 */
+
+public class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>();
+        for (int j = 0; j <= n - 1; j++) {
+        	pq.offer(new Tuple(0, j, matrix[0][j]));	
+        }
+        for (int i = 0; i < k - 1; i++) {
+            Tuple t = pq.poll();
+            if (t.x == n - 1) {
+            	continue;
+            }
+            pq.offer(new Tuple(t.x + 1, t.y, matrix[t.x + 1][t.y]));
+        }
+        return pq.poll().val;
+    }
+}
+
+class Tuple implements Comparable<Tuple> {
+    int x, y, val;
+    public Tuple (int x, int y, int val) {
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+    
+    @Override
+    public int compareTo (Tuple that) {
+        return this.val - that.val;
+    }
+}
