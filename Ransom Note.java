e5f38230c4879fa460b4ea713906fd267741449a
SaiMa <supermarkioner@gmail.com>
2016-08-20 13:21:04 +1000

    update

--- Ransom Note.java
+++ Ransom Note.java
@@ -16,5 +16,20 @@
     
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/53864/java-o-n-solution-easy-to-understand
 */
+
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--arr[ransomNote.charAt(i)-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
