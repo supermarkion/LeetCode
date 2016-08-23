007790cba392b3a8cc8da0f491c3e1216fa95af6
SaiMa <supermarkioner@gmail.com>
2016-08-23 21:39:40 +1000

    update

--- Shuffle an Array.java
+++ Shuffle an Array.java
@@ -19,5 +19,45 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/54885/easy-to-understand-solution-java
 */
+
+import java.util.Random;
+
+public class Solution {
+    int[] origin;
+    Random rand;
+    
+    public Solution(int[] nums) {
+        origin = nums;
+        rand = new Random();
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return origin;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        if (origin == null) {
+            return null;
+        }
+        int n = origin.length;
+        int[] dest = origin.clone();
+        for (int i = n - 1; i >= 0; i--){
+            int index = rand.nextInt(i + 1);
+            int temp = dest[i];
+            dest[i] = dest[index];
+            dest[index] = temp;
+        }
+        return dest;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
