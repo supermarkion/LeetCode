360bc39a801a56ae3c4e7d72e60017f351657f1c
SaiMa <supermarkioner@gmail.com>
2016-08-25 19:37:51 +1000

    update

--- Mini Parser.java
+++ Mini Parser.java
@@ -25,5 +25,57 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/54516/java-iterative-and-recursive-including-one-pass-solution
 */
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    int idx;
+    public NestedInteger deserialize(String s) {
+        idx = 0;
+        return helper(s);
+    }
+    NestedInteger helper(String s) {
+        NestedInteger ans = new NestedInteger();
+        if (s.charAt(idx) != '[') {
+            int start = idx;
+            while (idx < s.length() && s.charAt(idx) != ',' && s.charAt(idx) != ']') {
+                ++idx;   
+            }
+            ans.setInteger(Integer.parseInt(s.substring(start, idx--)));
+        } else {
+            for (++idx; idx < s.length() && s.charAt(idx) != ']'; ++idx)
+                if (s.charAt(idx) != ',') {
+                    ans.add(helper(s));    
+                }
+        }
+        return ans;
+    }
+}
