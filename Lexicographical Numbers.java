6b378d3e9e7f8b8e3e2f0589b2f097147456d72b
SaiMa <supermarkioner@gmail.com>
2016-08-27 10:29:15 +1000

    update

--- Lexicographical Numbers.java
+++ Lexicographical Numbers.java
@@ -11,5 +11,26 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/55184/java-o-n-time-o-1-space-iterative-solution-130ms
 */
+
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        int curr = 1;
+        for (int i = 1; i <= n; i++) {
+            list.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else if (curr % 10 != 9 && curr + 1 <= n) {
+                curr++;
+            } else {
+                while ((curr / 10) % 10 == 9) {
+                    curr /= 10;
+                }
+                curr = curr / 10 + 1;
+            }
+        }
+        return list;
+    }
+}
