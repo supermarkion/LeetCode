f1ff4e501cc9d5e1239ddca83d80065f9657d6f3
SaiMa <supermarkioner@gmail.com>
2016-08-28 12:37:28 +1000

    update

---
+++ First Unique Character in a String.java
@@ -0,0 +1,16 @@
+/*
+    Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+    Link: https://leetcode.com/problems/first-unique-character-in-a-string/
+
+    Example: 
+    	s = "leetcode"
+		return 0.
+
+		s = "loveleetcode",
+		return 2.
+
+    Solution: None
+
+    Source: None
+*/
