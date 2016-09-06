64c2276525477a5d392fae2d8995d74e4bf5ddc7
SaiMa <supermarkioner@gmail.com>
2016-09-06 18:46:42 +1000

    update

--- Longest Substring with At Least K Repeating Characters.java
+++ Longest Substring with At Least K Repeating Characters.java
@@ -26,5 +26,48 @@
 
 			The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/57077/java-solution-using-2d-array-to-store-letter-occurrences-17ms-o-n
 */
+
+public class Solution {
+    public int longestSubstring(String s, int k) {
+        if (s == null || s.length() < 1) {
+        	return 0;
+        }
+
+        int[][] letters = new int[26][s.length() + 1];
+        for (int i = 0; i < s.length(); i++) {
+            for (int c = 0; c < 26; c++) {
+                letters[c][i + 1] = letters[c][i];
+            }
+            letters[s.charAt(i) - 'a'][i + 1] += 1;
+        }
+        // May also optimize by deleting letters entries with 0 at end
+        
+        int longest = 0;
+        for (int start = 0; start < s.length(); start++) {
+            if (longest >= s.length() - start) {
+            	return longest;
+            }
+            for (int end = s.length(); end > start; end--) {
+                boolean works = true;
+                for (int c = 0; c < 26; c++) {
+                    int num = letters[c][end] - letters[c][start];
+                    if (num < k && num > 0) {
+                        works = false;
+                        end = s.indexOf('a' + c, start) + 1;
+                        break;
+                    }
+                }
+                if (works) {
+                    if (end - start > longest) {
+                    	longest = end - start;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return longest;
+    }
+}
