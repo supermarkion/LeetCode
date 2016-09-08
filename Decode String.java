62cdafcc827202c95a7f92ea7b04a46d183b7169
SaiMa <supermarkioner@gmail.com>
2016-09-08 19:58:09 +1000

    update

--- Decode String.java
+++ Decode String.java
@@ -14,5 +14,48 @@
 		s = "3[a2[c]]", return "accaccacc".
 		s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/57840/java-3ms-recursive-solution-no-stack
 */
+
+public class Solution {
+    public String decodeString(String s) {
+        return explore(s, 0, s.length());
+    }
+    
+    String explore(String s, int start, int end) {
+        if (start >= end) return "";
+        int time = 0;
+        int i = start;
+        //calculate repeating time
+        while (i < end && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            time = time * 10 + (int)(s.charAt(i++)-'0');
+        }
+        StringBuilder sb = new StringBuilder();
+        int nextstart = i;
+        if (time == 0) {
+            //don't need to repeat
+            while (i < end && (s.charAt(i) >= '9' || s.charAt(i) <= '0')) {
+                sb.append(s.charAt(i++));
+            }
+        } else {
+            int cntBracket = 1;
+            i++;
+            int newstart = i;
+            while (i < end && cntBracket > 0) {
+                if (s.charAt(i) == ']') {
+                    --cntBracket;
+                } else if (s.charAt(i) == '[') {
+                    ++cntBracket;
+                }
+                i++;
+            }
+            String inside = explore(s, newstart, i-1);
+            //repaet the inside string
+            while (time-- > 0) {
+                sb.append(inside);
+            }
+        }
+        sb.append(explore(s, i, end));
+        return sb.toString();     
+    }
+}
