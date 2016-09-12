6768320412c86bb7dd1297941e98dfa88cb069b6
SaiMa <supermarkioner@gmail.com>
2016-09-12 20:32:19 +1000

    update

--- Is Subsequence.java
+++ Is Subsequence.java
@@ -19,5 +19,29 @@
 			s = "axc", t = "ahbgdc"
 			Return false.
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/57205/java-only-2ms-much-faster-than-normal-2-pointers
 */
+
+public class Solution {
+
+    public boolean isSubsequence(String s, String t) {
+
+        if (t.length() < s.length()) {
+        	return false;
+        }
+        int prev = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            char tempChar = s.charAt(i);
+            prev = t.indexOf(tempChar,prev);
+            if (prev == -1) {
+            	return false;
+            }
+            prev++;
+        }
+        
+        return true;
+
+    }
+}
