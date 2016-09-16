b984656cfbf58e6fcaf9500a910fdeebff351792
SaiMa <supermarkioner@gmail.com>
2016-09-16 16:38:54 +1000

    update

--- Random Pick Index.java
+++ Random Pick Index.java
@@ -18,5 +18,39 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/58467/java-o-n-variant-of-reservoir-sampling
 */
+
+public class Solution {
+    
+    private int[] nums;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+    }
+    
+    private Random r = new Random();
+    
+    public int pick(int target) {
+        int ret = -1;
+        if (nums == null) {
+            return ret;
+        }
+        int upbound = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (r.nextInt(upbound) == 0) {
+                    ret = i;
+                } 
+                upbound++;
+            }
+        }
+        return ret;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
