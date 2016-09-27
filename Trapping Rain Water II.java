e69e90cee29d3e86ee4a60df0be0f842ee8bac78
SaiMa <supermarkioner@gmail.com>
2016-09-27 19:13:39 +1000

    update

---
+++ Trapping Rain Water II.java
@@ -0,0 +1,21 @@
+/*
+    Given an m x n matrix of positive integers representing the height of each unit cell in a 2D elevation map, compute the volume of water it is able to trap after raining.
+
+    Link: https://leetcode.com/problems/trapping-rain-water-ii/
+
+    Example: 
+		Given the following 3x6 height map:
+		[
+		  [1,4,3,1,3,2],
+		  [3,2,1,3,2,4],
+		  [2,3,3,2,3,1]
+		]
+
+		The above image represents the elevation map [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]] before the rain.
+		After the rain, water are trapped between the blocks. The total volume of water trapped is 4.
+		Return 4.
+
+    Solution: None
+
+    Source: None
+*/
