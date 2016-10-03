20954d7d6c0a8de2ede76c4cec432cf236437cca
SaiMa <supermarkioner@gmail.com>
2016-10-03 11:04:34 +1100

    update

---
+++ Split Array Largest Sum.java
@@ -0,0 +1,25 @@
+/*
+    Given an array which consists of non-negative integers and an integer m, you can split the array into m non-empty 
+    continuous subarrays. Write an algorithm to minimize the largest sum among these m subarrays.
+    
+    Note:
+        Given m satisfies the following constraint: 1 ≤ m ≤ length(nums) ≤ 14,000.
+    Link: https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+
+    Example: 
+		Input:
+        nums = [1,2,3,4,5]
+        m = 2
+
+        Output:
+        9
+
+        Explanation:
+        There are four ways to split nums into two subarrays.
+        The best way is to split it into [1,2,3] and [4,5],
+        where the largest sum among the two subarrays is only 9.
+
+    Solution: None
+
+    Source: None
+*/
