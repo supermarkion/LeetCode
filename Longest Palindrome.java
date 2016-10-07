600de975e74534297d1a55b8e03d96e99b922a53
SaiMa <supermarkioner@gmail.com>
2016-10-07 20:03:27 +1100

    update

--- Longest Palindrome.java
+++ Longest Palindrome.java
@@ -19,5 +19,48 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/61821/my-java-solution
 */
+
+ import java.util.*;
+ import java.util.Map.Entry;
+ 
+ public class Solution {
+    public int longestPalindrome(String s) {
+        int sum = 0;
+        Hashtable<Character,Integer> table = new Hashtable();
+        for (int i = 0; i < s.length(); i++) {
+            Integer count = table.get(s.charAt(i));
+            if (count != null) {
+                table.put(s.charAt(i), ++count);
+            } else {
+                table.put(s.charAt(i), 1);
+            }
+                
+        }
+        if (table.size() == 1) {
+            return s.length();
+        }
+            
+        Set<Entry<Character, Integer>> entrySet = table.entrySet();
+        boolean v = true;
+        for (Entry<Character,Integer> entry : entrySet) {   
+            int tmp = 0;
+            tmp = entry.getValue();
+            if (tmp % 2 == 0) {
+                sum += tmp;
+            } else if(tmp >2) {
+                if (v == true) {
+                    sum += tmp;
+                    v = false;
+                } else {
+                    sum += tmp - 1;
+                }
+            } else if ((tmp == 1) &&(v == true)) {
+                sum++;
+                v = false;
+            }
+        }
+        return sum;
+    }
+}
