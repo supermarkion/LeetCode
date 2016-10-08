cb096cb50dc94b6ba81ed1b35ba0ad042a126935
SaiMa <supermarkioner@gmail.com>
2016-10-08 11:49:30 +1100

    update

---
+++ Sum of Left Leaves.java
@@ -0,0 +1,19 @@
+/*
+    Find the sum of all left leaves in a given binary tree.
+
+    Link: https://leetcode.com/problems/sum-of-left-leaves/
+
+    Example: 
+
+            3
+           / \
+          9  20
+            /  \
+           15   7
+
+        There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+    Solution: None
+
+    Source: None
+*/
