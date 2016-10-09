bab1bc96239aafae94d802cd341e5707f0e0694d
SaiMa <supermarkioner@gmail.com>
2016-10-09 12:06:49 +1100

    update

--- Sum of Left Leaves.java
+++ Sum of Left Leaves.java
@@ -15,5 +15,37 @@
 
     Solution: None
 
-    Source: None
+    Source: https://leetcode.com/problems/sum-of-left-leaves/
 */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int toRet;
+    public int sumOfLeftLeaves(TreeNode root) 
+    {
+        toRet = 0;
+        dfs(root, -1);
+        return toRet;
+    }
+    void dfs(TreeNode root, int dir)
+    {
+        if (root == null) {
+          return;
+        }
+            
+        if (dir == 0 && root.left == null && root.right == null) {
+            toRet += root.val;
+            return;
+        }
+        dfs(root.left, 0);
+        dfs(root.right, 1);
+    }
+}
