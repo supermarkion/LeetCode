ef53a722316948c8aa584ab359239111c99b24b0
Sai Ma <supermarkioner@gmail.com>
2016-10-18 21:20:08 +1100

    update

--- Longest Repeating Character Replacement.java
+++ Longest Repeating Character Replacement.java
@@ -32,5 +32,22 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/63494/java-12-lines-o-n-sliding-window-solution-with-explanation/2
 */
+
+public class Solution {
+    public int characterReplacement(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}
