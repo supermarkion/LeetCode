248c3a639c6a4789d2f97913ae6bfed9eb35c7a8
Sai Ma <supermarkioner@gmail.com>
2016-10-21 20:01:39 +1100

    update

--- Reconstruct Original Digits from English.java
+++ Reconstruct Original Digits from English.java
@@ -20,5 +20,34 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/63386/one-pass-o-n-java-solution-simple-and-clear
 */
+
+public class Solution {
+    static String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    static char[] ids = {'z', 'o', 'w', 'h', 'u', 'f', 'x', 's', 'g', 'i'};
+    static int[] order = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+    
+    public String originalDigits(String s) {
+        int[] dCount = new int[10], lCount = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            lCount[c - 'a']++;
+        }
+
+        for (int d : order) {
+            dCount[d] = lCount[ids[d] - 'a'];
+            for (char c : digits[d].toCharArray()) {
+                lCount[c - 'a'] -= dCount[d];
+            } 
+        }
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < dCount[i]; ++j) {
+                ans.append(i);
+            }
+        }
+        return ans.toString();
+    }
+}
