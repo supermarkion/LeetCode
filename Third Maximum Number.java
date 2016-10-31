1be2b8b36ae0f1721d8997c5f52456e8fa2ededb
Sai Ma <supermarkioner@gmail.com>
2016-10-31 19:15:15 +1100

    update

--- Third Maximum Number.java
+++ Third Maximum Number.java
@@ -26,5 +26,28 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/65119/java-solution-using-treeset
 */
+
+public class Solution {
+    public final int N = 3;
+    public int thirdMax(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                continue;
+            }
+            if (set.size() < N || nums[i] > set.first()) {
+                if (set.size() == N) {
+                    set.remove(set.first());
+                }
+                set.add(nums[i]);
+            }
+        }
+        return set.size() == N ? set.first() : set.last();
+    }
+}
