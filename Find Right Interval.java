706c7a54fbd2fcc96fb55295bb193465bc5e87f6
Sai Ma <supermarkioner@gmail.com>
2016-11-04 18:52:45 +1100

    update

--- Find Right Interval.java
+++ Find Right Interval.java
@@ -35,5 +35,32 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/65817/java-clear-o-n-logn-solution-based-on-treemap
 */
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public int[] findRightInterval(Interval[] intervals) {
+        int[] result = new int[intervals.length];
+        java.util.NavigableMap<Integer, Integer> intervalMap = new TreeMap<>();
+        
+        for (int i = 0; i < intervals.length; ++i) {
+            intervalMap.put(intervals[i].start, i);    
+        }
+        
+        for (int i = 0; i < intervals.length; ++i) {
+            Map.Entry<Integer, Integer> entry = intervalMap.ceilingEntry(intervals[i].end);
+            result[i] = (entry != null) ? entry.getValue() : -1;
+        }
+        
+        return result;
+    }
+}
