1403897828faf9b55bf33d9a68f939ddc6b45972
Sai Ma <supermarkioner@gmail.com>
2016-11-06 10:04:51 +1100

    update

--- Non-overlapping Intervals.java
+++ Non-overlapping Intervals.java
@@ -25,5 +25,35 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/66391/java-concise-code-greedy-method
 */
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        if (intervals.length < 1){
+            return 0;
+        }
+        
+        Arrays.sort(intervals, ((a, b) -> (a.end - b.end)));
+        int sol = 0;
+        int firstEnd = intervals[0].start;
+        for (Interval interval : intervals) {
+            if (firstEnd <= interval.start) {
+                firstEnd = interval.end;
+            }
+            else {
+                sol++;
+            }
+        }
+        return sol;
+    }
+}
