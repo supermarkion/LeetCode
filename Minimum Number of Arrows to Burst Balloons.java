c6c0e8c63e6db829e38ec3ec75a9bf901b574a1b
Sai Ma <supermarkioner@gmail.com>
2016-11-10 21:56:57 +1100

    update

--- Minimum Number of Arrows to Burst Balloons.java
+++ Minimum Number of Arrows to Burst Balloons.java
@@ -23,5 +23,36 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/66579/java-greedy-soution/2
 */
+
+public class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0 || points[0].length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if (a[0] == b[0]) {
+                    return a[1] - b[1];
+                }
+                else {
+                    return a[0] - b[0];
+                }
+            }
+        });
+        
+        int minArrows = 1;
+        int arrowLimit = points[0][1];
+        for (int i = 1;i < points.length; i++) {
+            int[] baloon = points[i];
+            if (baloon[0] <= arrowLimit) {
+                arrowLimit = Math.min(arrowLimit, baloon[1]);
+            } else {
+                minArrows++;
+                arrowLimit = baloon[1];
+            }
+        }
+        return minArrows;
+    }
+}
