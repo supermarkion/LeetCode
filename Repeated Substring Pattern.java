747a1b86f61c1b403105901f8e6eee2bf25627dd
Sai Ma <supermarkioner@gmail.com>
2016-11-18 17:07:27 +1100

    update

--- Repeated Substring Pattern.java
+++ Repeated Substring Pattern.java
@@ -23,5 +23,26 @@
             
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/67992/java-simple-solution-with-explanation
 */
+
+public class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+        int l = str.length();
+        for (int i = l / 2;i >= 1; i--) {
+            if (l % i == 0) {
+                int m = l / i;
+                String subS = str.substring(0,i);
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < m; j++) {
+                    sb.append(subS);
+                }
+                if ( sb.toString().equals(str)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+            
+    }
+}
