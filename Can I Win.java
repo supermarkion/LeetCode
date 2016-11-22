fc3abd96c2cb85aac5bd167d4fa41c1b086b1d07
Sai Ma <supermarkioner@gmail.com>
2016-11-22 18:18:50 +1100

    update

--- Can I Win.java
+++ Can I Win.java
@@ -32,5 +32,48 @@
             
     Solution: None
 
-    Source: None
+    Source: https://leetcode.com/problems/can-i-win/
 */
+
+public class Solution {
+    private Boolean[] win;
+    int choosen = 0;
+
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (desiredTotal == 0) {
+            return true;
+        }
+        if ((1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal) {
+            return false;
+        }
+        win = new Boolean[1 << maxChoosableInteger];
+        return canWin(maxChoosableInteger, desiredTotal, 0);
+    }
+
+    private boolean canWin(int n, int total, int now) {
+        if (win[choosen] != null) {
+        	return win[choosen];
+        }
+            
+        if (now >= total) {
+            win[choosen] = false;
+            return false;
+        }
+        for (int i = 1; i <= n; i++) {
+            int bit = 1 << (i - 1);
+            if ((choosen & bit) == 0) {
+                choosen ^= bit;
+                boolean ulose = !canWin(n, total, now + i);
+                choosen ^= bit;
+
+                if (ulose) {
+                    win[choosen] = true;
+                    return true;
+                }
+            }
+        }
+        win[choosen] = false;
+        return false;
+    }
+
+}
