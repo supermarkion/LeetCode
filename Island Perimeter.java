458ca0a5469c3eeb634244eb923be6e5e6c47b64
Sai Ma <supermarkioner@gmail.com>
2016-11-24 08:06:15 +1100

    update

--- Island Perimeter.java
+++ Island Perimeter.java
@@ -19,5 +19,27 @@
             
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/68786/clear-and-easy-java-solution/2
 */
+
+public class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int islands = 0, neighbours = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    islands++; // count islands
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+                    	neighbours++; // count down neighbours	
+                    }
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) {
+                    	neighbours++; // count right neighbours	
+                    }
+                }
+            }
+        }
+
+        return islands * 4 - neighbours * 2;
+    }
+}
