736033351d9bfff4cf1d71c1fbc338a39ee8580c
Sai Ma <supermarkioner@gmail.com>
2016-11-26 00:35:29 +1100

    update

--- Minimum Moves to Equal Array Elements II.java
+++ Minimum Moves to Equal Array Elements II.java
@@ -21,5 +21,26 @@
             
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/68762/java-solution-with-thinking-process
 */
+
+public class Solution {
+    public int minMoves2(int[] nums) {
+        if (nums==null||  nums.length==0) {
+        	return 0;
+        }
+        long moves = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        long totalSum = 0L;
+        long sum = 0L;
+        for (int i =0;i<nums.length;i++) {
+            totalSum += (long)nums[i];
+        }
+        for(int i =0;i<nums.length;i++) {
+            long m = (long)(i - (nums.length - i - 1) - 1) * (long)nums[i] - sum + (totalSum - sum);
+            moves = Math.min(m, moves);
+            sum += nums[i];
+        }
+        return (int)moves;
+    }
+}
