62f3546444c8bc3a96d1d36ba2cb070b46bf20c3
Sai Ma <supermarkioner@gmail.com>
2016-12-04 09:18:36 +1100

    update

--- Battleships in a Board.java
+++ Battleships in a Board.java
@@ -25,5 +25,33 @@
     
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/62970/simple-java-solution
 */
+
+public class Solution {
+     public int countBattleships(char[][] board) {
+        int m = board.length;
+        if (m == 0) {
+        	return 0;
+        }
+        int n = board[0].length;
+        
+        int count =0 ;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                	continue;
+                }
+                if (i > 0 && board[i - 1][j] == 'X') {
+                	continue;
+                }
+                if (j > 0 && board[i][j -1 ] == 'X') {
+                	continue;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
