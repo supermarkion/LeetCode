369dd9d4afe69b423cb1673a6dabeef6ef1085a9
Sai Ma <supermarkioner@gmail.com>
2016-12-09 10:13:28 +1100

    update

---
+++ Number of Segments in a String.java
@@ -0,0 +1,17 @@
+/*
+	Count the number of segments in a string, where a segment is defined to be a contiguous 
+	sequence of non-space characters.
+
+	Please note that the string does not contain any non-printable characters.
+    
+    Link: https://leetcode.com/problems/number-of-segments-in-a-string/
+
+    Example: 
+
+		Input: "Hello, my name is John"
+		Output: 5
+            
+    Solution: None
+
+    Source: None
+*/
