c10db7d22e2a621aeca1790bd1559be85872b3ff
Sai Ma <supermarkioner@gmail.com>
2016-12-10 08:59:43 +1100

    update

--- Number of Segments in a String.java
+++ Number of Segments in a String.java
@@ -15,3 +15,15 @@
 
     Source: None
 */
+
+public class Solution {
+    public int countSegments(String s) {
+        int result = 0;
+        for (int i=0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+            	result++;        
+            }
+        }
+        return result;
+    }
+}
