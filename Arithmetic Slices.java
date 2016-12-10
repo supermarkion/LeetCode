1ad3ce8bef75cd6e16fbc68c275b617f3b4b549b
Sai Ma <supermarkioner@gmail.com>
2016-12-11 09:05:46 +1100

    update

--- Arithmetic Slices.java
+++ Arithmetic Slices.java
@@ -28,5 +28,19 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/63302/simple-java-solution-9-lines-2ms/5
 */
+
+public class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        int curr = 0, sum = 0;
+        for (int i = 2; i < A.length; i++)
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                curr += 1;
+                sum += curr;
+            } else {
+                curr = 0;
+            }
+        return sum;
+    }
+}
