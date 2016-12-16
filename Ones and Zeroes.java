13d7b25a4c93d27e0881369c04bf2863e3101c3f
supermarkioner <supermarkioner@gmail.com>
2016-12-16 23:21:00 +1100

    update

--- Ones and Zeroes.java
+++ Ones and Zeroes.java
@@ -1,11 +1,9 @@
 /*
     In the computer world, use restricted resource you have to generate maximum benefit is what we always want to pursue.
 
-	For now, suppose you are a dominator of m 0s and n 1s respectively. On the other hand, there is an array with strings 
-	consisting of only 0s and 1s.
+	For now, suppose you are a dominator of m 0s and n 1s respectively. On the other hand, there is an array with strings consisting of only 0s and 1s.
 
-	Now your task is to find the maximum number of strings that you can form with given m 0s and n 1s. Each 0 and 1 can 
-	be used at most once.
+	Now your task is to find the maximum number of strings that you can form with given m 0s and n 1s. Each 0 and 1 can be used at most once.
 
 	Note:
 		The given numbers of 0s and 1s will both not exceed 100
