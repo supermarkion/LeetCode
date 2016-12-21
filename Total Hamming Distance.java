3711faaaa0ac7f039741b97ad60ec0ddb6dffc87
supermarkioner <supermarkioner@gmail.com>
2016-12-21 12:34:04 +1100

    update

--- Total Hamming Distance.java
+++ Total Hamming Distance.java
@@ -15,5 +15,19 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/72092/java-o-n-time-o-1-space
 */
+
+public class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int total = 0, n = nums.length;
+        for (int j = 0; j < 32; j++) {
+            int bitCount = 0;
+            for (int i = 0; i < n; i++) {
+                bitCount += (nums[i] >> j) & 1;                
+            }
+            total += bitCount * (n - bitCount);
+        }
+        return total;    
+    }
+}
