a350fbad7e4a1ff1a93663a3e94ca00c7e00d4a9
supermarkioner <supermarkioner@gmail.com>
2016-12-22 10:34:03 +1100

    update

---
+++ Matchsticks to Square.java
@@ -0,0 +1,27 @@
+/*
+    Remember the story of Little Match Girl? By now, you know exactly what matchsticks the little match girl has, 
+    please find out a way you can make one square by using up all those matchsticks. You should not break any stick, 
+    but you can link them up, and each matchstick must be used exactly one time.
+
+	Your input will be several matchsticks the girl has, represented with their stick length. Your output will 
+	either be true or false, to represent whether you could make one square using all the matchsticks the little 
+	match girl has.te:
+    
+    Link: https://leetcode.com/problems/matchsticks-to-square/
+
+    Example: 
+    	
+    	Example 1:
+			Input: [1,1,2,2,2]
+			Output: true
+			Explanation: You can form a square with length 2, one side of the square came two sticks with length 1.
+
+		Example 2:
+			Input: [3,3,3,3,4]
+			Output: false
+			Explanation: You cannot find a way to form a square with all the matchsticks.
+
+    Solution: None
+
+    Source: None
+*/
