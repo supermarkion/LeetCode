06c827d171b3a26abba267e12acadd7c499fd206
supermarkioner <supermarkioner@gmail.com>
2016-12-29 14:25:32 +0800

    update

--- Sort Characters By Frequency.java
+++ Sort Characters By Frequency.java
@@ -34,5 +34,31 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/65947/o-n-easy-to-understand-java-solution/2
 */
+
+public class Solution {
+    public String frequencySort(String s) {
+        int[] freq = new int [256];
+        for (char ch: s.toCharArray()) {
+        	freq[ch]++;
+        }
+        TreeMap<Integer, List<Character>> tree = new TreeMap<Integer, List<Character>>();
+        for (int i=0; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                if (!tree.containsKey(freq[i])) {
+                    tree.put(freq[i], new LinkedList<Character>());
+                }
+                tree.get(freq[i]).add((char)i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while(tree.size() > 0) {
+            Map.Entry<Integer, List<Character>> entry = tree.pollLastEntry();
+            for (Character ch: entry.getValue()) {
+                sb.append(new String(new char[entry.getKey()]).replace('\0', ch));
+            }
+        }
+        return sb.toString();
+    }
+}
