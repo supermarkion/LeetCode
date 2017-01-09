fe4c4c20a507a875bee7d159940e7a9cc9dabe31
Sai Ma <supermarkioner@gmail.com>
2017-01-09 19:04:36 +1100

    update

--- Add Two Numbers II.java
+++ Add Two Numbers II.java
@@ -14,5 +14,61 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/74475/my-easy-understanding-java-recursive-solution
 */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int c = 0;   // it is the carry
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int len1 = getLength(l1);
+        int len2 = getLength(l2);
+        ListNode dummy = new ListNode(-1);
+        dummy.next = len1 >= len2 ? helper(l1, l2, len1-len2) : helper(l2, l1, len2-len1);
+        if (c != 0) {
+            ListNode newNode = new ListNode(c);
+            newNode.next = dummy.next;
+            dummy.next = newNode;
+        }
+        return dummy.next;
+    }
+    
+    int getLength(ListNode l) {
+        ListNode cur = l;
+        int c = 0;
+        while (cur != null) {
+            c++;
+            cur = cur.next;
+        }
+        return c;
+    }
+    
+    ListNode helper(ListNode l1, ListNode l2, int offset) {  // l1 is offset size header of l2
+        if (offset != 0) {
+            ListNode l = helper(l1.next, l2, offset - 1);
+            int tmp = l1.val + c;
+            ListNode newNode = new ListNode(tmp % 10);
+            newNode.next = l;
+            c = tmp / 10;
+            return newNode;
+        } else {
+            if (l1 != null && l2 != null) {
+                ListNode l = helper(l1.next, l2.next, offset);
+                int tmp = l1.val + l2.val + c;
+                ListNode newNode = new ListNode(tmp % 10);
+                newNode.next = l;
+                c = tmp / 10;
+                return newNode;
+            }
+        }
+        return null;
+    }
+}
