16850d59cd59e75aaf5dbb3b88dbb45d94049cc2
Sai Ma <supermarkioner@gmail.com>
2017-01-13 22:03:51 +1100

    update

--- License Key Formatting.java
+++ License Key Formatting.java
@@ -27,5 +27,17 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/74995/java-5-lines-clean-solution
 */
+
+public class Solution {
+     public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '-') {
+                sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
+            }
+        }
+        return sb.reverse().toString().toUpperCase();
+    } 
+}
