e438d7452a1dbd4c966a0c03eb15d9be52f89367
Sai Ma <supermarkioner@gmail.com>
2017-01-18 18:31:23 +1100

    update

---
+++ Max Consecutive Ones.java
@@ -0,0 +1,16 @@
+/*
+    Given a binary array, find the maximum number of consecutive 1s in this array.
+
+    Link: https://leetcode.com/problems/max-consecutive-ones/
+
+    Example: 
+
+        Input: [1,1,0,1,1,1]
+        Output: 3
+        Explanation: The first two digits or the last three digits are consecutive 1s.
+            The maximum number of consecutive 1s is 3.
+
+    Solution: None
+
+    Source: None
+*/
