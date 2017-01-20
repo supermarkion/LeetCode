52300e6362929ad837b4ddf50e578a3d5f171895
Sai Ma <supermarkioner@gmail.com>
2017-01-20 22:55:42 +1100

    update

---
+++ Sliding Window Median.java
@@ -0,0 +1,20 @@
+/*
+    Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle 
+    value. So the median is the mean of the two middle value.
+
+    Link: https://leetcode.com/problems/sliding-window-median/
+
+    Example: 
+
+        [2,3,4] , the median is 3
+
+        [2,3], the median is (2 + 3) / 2 = 2.5
+
+        Given an array nums, there is a sliding window of size k which is moving from the very left of the array 
+        to the very right. You can only see the k numbers in the window. Each time the sliding window moves right 
+        by one position. Your job is to output the median array for each window in the original array.
+
+    Solution: None
+
+    Source: None
+*/
