b6e2b6ba99740dc2f99bc41524752628ff1c6054
Sai Ma <supermarkioner@gmail.com>
2017-01-26 00:05:20 +1100

    update

---
+++ Increasing Subsequences.java
@@ -0,0 +1,14 @@
+/*
+    Given an integer array, your task is to find all the different possible increasing subsequences 
+    of the given array, and the length of an increasing subsequence should be at least 2 .
+
+    Link: https://leetcode.com/problems/increasing-subsequences/
+
+    Example: 
+        Input: [4, 6, 7, 7]
+        Output: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+
+    Solution: None
+
+    Source: None
+*/
