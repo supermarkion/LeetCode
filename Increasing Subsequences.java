6078fcac536df343dd2090d430cfd5a74e143e61
Sai Ma <supermarkioner@gmail.com>
2017-01-27 17:03:23 +1100

    update

--- Increasing Subsequences.java
+++ Increasing Subsequences.java
@@ -10,5 +10,28 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/76249/java-20-lines-backtracking-solution-using-set-beats-100
 */
+
+public class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+         Set<List<Integer>> res= new HashSet<List<Integer>>();
+         List<Integer> holder = new ArrayList<Integer>();
+         findSequence(res, holder, 0, nums);
+         List result = new ArrayList(res);
+         return result;
+     }
+
+    public void findSequence(Set<List<Integer>> res, List<Integer> holder, int index, int[] nums) {
+        if (holder.size() >= 2) {
+            res.add(new ArrayList(holder));
+        }
+        for (int i = index; i < nums.length; i++) {
+            if(holder.size() == 0 || holder.get(holder.size() - 1) <= nums[i]) {
+                holder.add(nums[i]);
+                findSequence(res, holder, i + 1, nums);
+                holder.remove(holder.size() - 1);
+            }
+        }
+    }
+}
