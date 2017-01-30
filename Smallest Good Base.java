afe23a24a60463c455aa732f5d15adcc8c2b45fc
Sai Ma <supermarkioner@gmail.com>
2017-01-30 19:11:51 +1100

    update

---
+++ Smallest Good Base.java
@@ -0,0 +1,28 @@
+/*
+    For an integer n, we call k>=2 a good base of n, if all digits of n base k are 1.
+
+	Now given a string representing n, you should return the smallest good base of n in string format. 
+
+    Link: https://leetcode.com/problems/smallest-good-base/
+
+	Example: 
+	
+		Example 1:
+			Input: "13"
+			Output: "3"
+			Explanation: 13 base 3 is 111.
+
+		Example 2:
+			Input: "4681"
+			Output: "8"
+			Explanation: 4681 base 8 is 11111.
+		
+		Example 3:
+			Input: "1000000000000000000"
+			Output: "999999999999999999"
+			Explanation: 1000000000000000000 base 999999999999999999 is 11.
+
+    Solution: None
+
+    Source: None
+*/
