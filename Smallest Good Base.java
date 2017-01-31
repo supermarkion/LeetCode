d447255035be29ee471709364727eca75d27ffc4
Sai Ma <supermarkioner@gmail.com>
2017-01-31 21:34:09 +1100

    update

--- Smallest Good Base.java
+++ Smallest Good Base.java
@@ -24,5 +24,34 @@
 
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/76406/java-c-binary-search-solutions-with-detailed-explanation
 */
+
+import java.math.*;
+
+public class Solution {
+    public String smallestGoodBase(String n) {
+        BigInteger N = new BigInteger(n);
+        long base = Long.MAX_VALUE;
+
+        for (int k = 2; k < 66; k++) {
+            long l = 2, r = Long.MAX_VALUE - 5;
+            while (l <= r) {
+                long mid = l + (r - l) / 2;
+                BigInteger cb = BigInteger.valueOf(mid).pow(k).subtract(BigInteger.ONE);
+                BigInteger wb = N.multiply(BigInteger.valueOf(mid).subtract(BigInteger.ONE));
+                int cmp = cb.compareTo(wb);
+                if (cmp == 0) {
+                    base = Math.min(base, mid);
+                    break;
+                } else if (cmp < 0) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return "" + base;
+    }
+}
