34f30a5364957236a7c7d3d914046bbac75decb0
Sai Ma <supermarkioner@gmail.com>
2017-02-01 21:39:32 +1100

    update

---
+++ Find Mode in Binary Search Tree.java
@@ -0,0 +1,25 @@
+/*
+    Given a binary search tree (BST) with duplicates, find all the mode(s) (the most frequently occurred element) in the given BST.
+
+	Assume a BST is defined as follows:
+
+	The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+	The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+	Both the left and right subtrees must also be binary search trees.
+
+    Link: https://leetcode.com/problems/find-mode-in-binary-search-tree/
+
+	Example: 
+	
+		Given BST [1,null,2,2],
+		   1
+		    \
+		     2
+		    /
+		   2
+		return [2].
+		
+    Solution: None
+
+    Source: None
+*/
