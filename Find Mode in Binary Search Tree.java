93bd17fe1f54609b077290f3c236da8cfafee895
Sai Ma <supermarkioner@gmail.com>
2017-02-02 21:43:59 +1100

    update

--- Find Mode in Binary Search Tree.java
+++ Find Mode in Binary Search Tree.java
@@ -21,5 +21,51 @@
 		
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/77079/java-ac-solution
 */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    Map<Integer, Integer> map; 
+    int max = 0;
+    public int[] findMode(TreeNode root) {
+        if (root == null) {
+        	return new int[0]; 
+        }
+        this.map = new HashMap<>(); 
+        
+        inorder(root); 
+        
+        List<Integer> list = new LinkedList<>();
+        for (int key : map.keySet()) {
+            if (map.get(key) == max) {
+            	list.add(key);
+            }
+        }
+        
+        int[] res = new int[list.size()];
+        for (int i = 0; i<res.length; i++) {
+        	res[i] = list.get(i);
+        }
+        return res; 
+    }
+    
+    private void inorder(TreeNode node){
+        if (node.left!=null) {
+        	inorder(node.left);
+        }
+        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+        max = Math.max(max, map.get(node.val));
+        if (node.right!=null) {
+        	inorder(node.right); 
+        }
+    }
+}
