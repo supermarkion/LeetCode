831d0364cf1c44969088ca9662e1152346fedafa
Sai Ma <supermarkioner@gmail.com>
2017-02-05 10:22:34 +1100

    update

---
+++ Number Complement.java
@@ -0,0 +1,21 @@
+/*
+	Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+    Link: https://leetcode.com/problems/number-complement/
+
+	Example: 
+	
+		Example 1:
+			Input: 5
+			Output: 2
+			Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+		
+		Example 2:
+			Input: 1
+			Output: 0
+			Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+		
+    Solution: None
+
+    Source: None
+*/
