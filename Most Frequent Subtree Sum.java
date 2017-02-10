cefaf9c7ce51699a745b9870e4af68b0e9cbf340
Sai Ma <supermarkioner@gmail.com>
2017-02-10 21:32:21 +1100

    update

--- Most Frequent Subtree Sum.java
+++ Most Frequent Subtree Sum.java
@@ -23,5 +23,41 @@
 		
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/77766/short-easy-java
 */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int max = 0;
+    public int[] findFrequentTreeSum(TreeNode root) {
+        if (root == null) {
+        	return new int[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        helper(root, map);
+        List<Integer> res = new LinkedList<>();
+        for (Map.Entry<Integer, Integer> me : map.entrySet()) {
+            if (me.getValue() == max) {
+				res.add(me.getKey());
+            }
+        }
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+    
+    private int helper(TreeNode n, Map<Integer, Integer> map){
+        int left = (n.left == null) ? 0 : helper(n.left, map);
+        int right = (n.right == null) ? 0 : helper(n.right, map);
+        int sum = left + right + n.val;
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        max = Math.max(max, map.get(sum));
+        return sum;
+    }
+}
