f23ec70c301162caf7a1c73c777f0984571021ca
Sai Ma <supermarkioner@gmail.com>
2017-02-14 21:56:06 +1100

    update

--- Find Largest Value in Each Tree Row.java
+++ Find Largest Value in Each Tree Row.java
@@ -16,5 +16,43 @@
 				
     Solution: None
 
-    Source: None
+    Source: https://discuss.leetcode.com/topic/79278/java-solution
 */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+       List<Integer> result = new LinkedList<>();
+    	if (root == null) {
+    		return result;
+    	}
+    	Queue<TreeNode> queue = new LinkedList<>();
+    	queue.offer(root);
+    	while (!queue.isEmpty()){
+	        int level = queue.size();
+	        int max_value = Integer.MIN_VALUE;
+	        for (int i = 0; i < level; i++){
+	            TreeNode node = queue.poll();
+	            if (node.val > max_value) {
+	                max_value = node.val;
+	            }
+	            if (node.left != null) {
+	            	queue.offer(node.left);
+	            }
+	            if (node.right != null) {
+	            	queue.offer(node.right);
+	            }
+	        }
+	        result.add(max_value);
+	    }
+    	return result;
+    }
+}
