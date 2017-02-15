47b1e2ac59a4d90d1d326e69ceb0aa8754738bb9
Sai Ma <supermarkioner@gmail.com>
2017-02-15 21:31:34 +1100

    update

---
+++ Find Bottom Left Tree Value.java
@@ -0,0 +1,34 @@
+/*
+	Given a binary tree, find the leftmost value in the last row of the tree.
+
+    Link: https://leetcode.com/problems/find-bottom-left-tree-value/
+
+	Example: 
+		Example 1:
+		Input:
+
+		    2
+		   / \
+		  1   3
+
+		Output:
+		1
+		
+		Example 2: 
+		Input:
+
+		        1
+		       / \
+		      2   3
+		     /   / \
+		    4   5   6
+		       /
+		      7
+
+		Output:
+		7
+				
+    Solution: None
+
+    Source: None
+*/
