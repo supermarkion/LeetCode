bb83f3ef7ba6b6b185d5795bcbc95c91b441bdc3
Sai Ma <supermarkioner@gmail.com>
2017-02-19 09:57:19 +1100

    update

---
+++ Base 7.java
@@ -0,0 +1,18 @@
+/*
+	Given an integer, return its base 7 string representation.
+    
+    Link: https://leetcode.com/problems/base-7/?tab=Description
+
+	Example: 
+		Example 1:
+		Input: 100
+		Output: "202"
+		
+		Example 2:
+		Input: -7
+		Output: "-10"
+				
+    Solution: None
+
+    Source: None
+*/
